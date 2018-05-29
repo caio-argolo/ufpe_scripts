aa6d6e31e5791eae69e62d3a47adf921f89566f3
caio-argolo <39710936+caio-argolo@users.noreply.github.com>
2018-05-29 08:39:27 -0300

    Add files via upload
    2 scripts upados primeiro por teste

---
+++ test_01.py
@@ -0,0 +1,14 @@
+#!/home/caio/anaconda3/bin/python3
+
+nome = 'Ubaldo'
+
+# Usar o f permite que ele reconheca o conteudo da variavel dentro de {}
+print(f'Olar {nome}')
+
+print('Olar {nome}')
+
+# String methods
+
+long_string = "  Caio esta em Recife  "
+
+print(long_string.lstrip() + ", e nao em Ilheus")
---
+++ test_import.py
@@ -0,0 +1,6 @@
+#!/home/caio/anaconda3/bin/python3
+
+a = 'food'
+b = 'water'
+c = 'tchengus'
+print (a, b, c)
